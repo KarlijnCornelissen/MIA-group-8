abb80a35e490b376c2479d3b9b60e3884ea9b004
maritjanssen <m.janssen@student.tue.nl>
2024-09-29 10:11:25 +0200

    Genormaliseerde versie aanmaken

--- analysis.py
+++ analysis.py
@@ -1,4 +1,26 @@
 #Data analysis 
 import pandas as pd
-df_CC_test=pd.read_csv("MIA-group-8\code\image_data\CC_data_test.csv",index_col=[0,1])
+import numpy as np
+df_CC_test=pd.read_csv("MIA-group-8\code\CC_data_test.csv",index_col=[0,1])
 print(df_CC_test)
+
+# Creating normalized version:
+def normalized_data(title):
+    df=pd.read_csv("{title}.csv")
+    df_normalized = pd.DataFrame(np.nan, index=df.index, columns=df.columns)
+
+    for i in df.index:
+        df_normalized.iloc[i, 0] = df.iloc[i, 0]
+        for j in range(1,df.columns+1):
+            df_normalized.iloc[i,j]=(df.iloc[i,j])/(df.iloc[i,0])
+
+
+
+    normalized_title = f"{title}_normalized"
+    file_path = f"MIA-group-8/code/{normalized_title}.csv"
+
+    # Save the DataFrame to a CSV file
+    df_normalized.to_csv(file_path)
+
+
+normalized_data('CC_data_test')
