ac5fd1c1de0e790de0282bd24c67906c55ae7f94
Luukbrouwer <luuk904@gmail.com>
2024-09-29 13:04:24 +0200

    plotjes genereren

--- analysis.py
+++ analysis.py
@@ -1,26 +1,47 @@
 #Data analysis 
 import pandas as pd
 import numpy as np
-df_CC_test=pd.read_csv("MIA-group-8\code\CC_data_test.csv",index_col=[0,1])
-print(df_CC_test)
+import seaborn as sns
+import matplotlib.pyplot as plt
+df_CC=pd.read_csv("MIA-group-8\code\CC_data.csv", index_col=[0,1])
+
 
 # Creating normalized version:
 def normalized_data(title):
-    df=pd.read_csv(f"{title}.csv")
-    df_normalized = pd.DataFrame(np.nan, index=df.index, columns=df.columns)
+    df=pd.read_csv(f"MIA-group-8\code\{title}.csv", index_col=[0,1])
+    df.reset_index(drop=True)
 
-    for i in df.index:
-        df_normalized.iloc[i, 0] = df.iloc[i, 0]
-        for j in range(1,df.columns+1):
-            df_normalized.iloc[i,j]=(df.iloc[i,j])/(df.iloc[i,0])
-
-
+    index_names= [f"image: {i}" for i in range(1,len(df.iloc[:,0])+1)]
+    df["Image number"]=index_names
+    
+    df.set_index("Image number",inplace=True)
+    df.columns = ["Original images", "high noise", "Low noise", "large filter", "small filter"]
+    for i in index_names:
+        for j in df.columns:
+            df.loc[i]=(df.loc[i])-(df.loc[i,"Original images"])
+    # s=[f"image: {i}" for i in range(1,7)]
+    # df=df.loc[s]
 
     normalized_title = f"{title}_normalized"
     file_path = f"MIA-group-8/code/{normalized_title}.csv"
 
     # Save the DataFrame to a CSV file
-    df_normalized.to_csv(file_path)
+    df.to_csv(file_path)
+    return df
 
 
-normalized_data('CC_data_test')
+
+
+def make_boxplots(df):
+    sns.set_theme(style="ticks", palette="pastel")
+    print(pd.melt(df))
+
+    
+    # Draw a nested boxplot to show bills by day and time
+    sns.catplot(x="variable", y="value",
+                data=pd.melt(df), kind="violin", hue="variable")
+    plt.show()
+
+df_normalized = normalized_data('CC_data')
+make_boxplots(df_normalized)
+
