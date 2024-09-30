170741edea1c29bf582a4e6932933b1fbeac343b
Luukbrouwer <luuk904@gmail.com>
2024-09-30 16:01:36 +0200

    plots

--- analysis.py
+++ analysis.py
@@ -18,9 +18,12 @@ def normalized_data(title):
     df.columns = ["Original images", "high noise", "Low noise", "large filter", "small filter"]
     for i in index_names:
         for j in df.columns:
-            df.loc[i]=(df.loc[i])-(df.loc[i,"Original images"])
-    # s=[f"image: {i}" for i in range(1,7)]
-    # df=df.loc[s]
+            df.loc[i]=(df.loc[i])/(df.loc[i,"Original images"])
+
+    # if True: #title == "MI":
+    #     s=[f"image: {i}" for i in range(1,7)]
+    #     df=df.loc[s]
+
 
     normalized_title = f"{title}_normalized"
     file_path = f"MIA-group-8/code/{normalized_title}.csv"
@@ -32,16 +35,54 @@ def normalized_data(title):
 
 
 
-def make_boxplots(df):
+def make_boxplots(df_CC, df_MI):
     sns.set_theme(style="ticks", palette="pastel")
-    print(pd.melt(df))
+    
 
     
     # Draw a nested boxplot to show bills by day and time
-    sns.catplot(x="variable", y="value",
-                data=pd.melt(df), kind="violin", hue="variable")
-    plt.show()
+    # sns.catplot(x="variable", y="value",
+    #             data=pd.melt(df), kind="violin", hue="variable")
+    # plt.show()
 
-df_normalized = normalized_data('CC_data')
-make_boxplots(df_normalized)
+    fig = plt.figure(figsize=(6, 6))
+    ax1= plt.subplot(211)
+    
+    
+    
+    #CC:
+    df_CC = pd.melt(df_CC)
+    df_CC.columns = ["Image type", "Normalized Similarity" ]
+
+    sns.stripplot(ax=ax1, 
+        data=df_CC, x="Image type", y="Normalized Similarity", hue="Image type",
+        )
+    sns.pointplot(ax=ax1,
+        data=df_CC, x="Image type", y="Normalized Similarity", hue="Image type",
+        dodge=.8 - .8 / 3, palette="dark", errorbar=None,
+        markers="d", markersize=4, linestyle="none",
+        )
+    # MI:
+    ax2 = plt.subplot(212)
+    df_MI = pd.melt(df_MI)
+    df_MI.columns = ["Image type", "Normalized Similarity" ]
+
+    sns.stripplot(ax=ax2,
+        data=df_MI, x="Image type", y="Normalized Similarity", hue="Image type",
+        )
+    sns.pointplot(ax=ax2,
+        data=df_MI, x="Image type", y="Normalized Similarity", hue="Image type",
+        dodge=.8 - .8 / 3, palette="dark", errorbar=None,
+        markers="d", markersize=4, linestyle="none",
+        ) 
+    
+    sns.despine(left=True)
+    plt.show()
+    
+
+
+df_normalized_MI = normalized_data('MI_data')
+df_normalized_CC = normalized_data('CC_data')
+
+make_boxplots(df_normalized_CC,df_normalized_MI)
 
